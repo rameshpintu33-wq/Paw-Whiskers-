2ef5567dbaa127410b45a1067dde4707854b61f1
rameshpintu33-wq <rameshpintu33@gmail.com>
2026-02-12 23:40:40 +0530

    Add click events for dog and cat product alerts

---
+++ First.js
@@ -0,0 +1,8 @@
+// optional simple actions
+document.querySelector(".dog").onclick = () => {
+  alert("Dog products coming soon 🐶");
+};
+
+document.querySelector(".cat").onclick = () => {
+  alert("Cat products coming soon 🐱");
+};
